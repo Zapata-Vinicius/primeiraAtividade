1e005050dc99b0f63aad0a2e31b5871641f8ce2a
Vinicius Zapata <viniciusmz@al.insper.edu.br>
2026-08-28 17:51:35 -0300

    Modificando para que edit e delete funcionem

--- servidor.py
+++ servidor.py
@@ -1,65 +1,49 @@
-import sqlite3
 from flask import Flask, render_template_string, request, redirect
 import views
 
-with sqlite3.connect("banco.db") as connection:
-    cursor = connection.cursor()
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS note (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            title TEXT NOT NULL,
-            content TEXT NOT NULL,
-            is_favorite INTEGER DEFAULT 0
-        )
-    """
-    )
-    connection.commit()
+
 
 app = Flask(__name__)
-app.static_folder = "static"
 
+# Configurando a pasta de arquivos estáticos
+app.static_folder = 'static'
 
-@app.route("/")
+@app.route('/')
 def index():
+
     return render_template_string(views.index())
 
-
-@app.route("/submit", methods=["POST"])
+@app.route('/submit', methods=['POST'])
 def submit_form():
-    titulo = request.form.get("titulo")
-    detalhes = request.form.get("detalhes")
+    titulo = request.form.get('titulo')  # Obtém o valor do campo 'titulo'
+    detalhes = request.form.get('detalhes')  # Obtém o valor do campo 'detalhes'
 
     views.submit(titulo, detalhes)
-    return redirect("/")
+    return redirect('/')
 
-
-@app.route("/update/<int:id>", methods=["GET"])
+@app.route('/update/<int:id>', methods=['GET'])
 def edit_note(id):
     return render_template_string(views.edit(id))
 
-
-@app.route("/update", methods=["POST"])
+@app.route('/update', methods=['POST'])
 def update_note():
-    id = request.form.get("id")
-    titulo = request.form.get("titulo")
-    detalhes = request.form.get("detalhes")
-
+    id = request.form.get('id')
+    titulo = request.form.get('titulo')
+    detalhes = request.form.get('detalhes')
+    
     views.update(id, titulo, detalhes)
-    return redirect("/")
-
+    return redirect('/')
 
 @app.route("/delete/<int:id>", methods=["POST", "GET"])
 def delete_noteid(id):
     views.delete(id)
     return redirect("/")
 
-
-@app.route("/favorite/<int:id>", methods=["POST", "GET"])
+@app.route("/favorite/<int:id>", methods=["POST","GET"])
 def favorite_note(id):
     views.favorite(id)
     return redirect("/")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
